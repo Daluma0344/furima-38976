320511ba357d2b33af5ed5eb85da513ed779c8b1
Daluma0344 <japan_masa11@icloud.com>
2023-03-28 17:13:16 +0900

    半角英数字

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,14 +25,6 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include('Email is invalid')
     end
 
-    it "emailが一意性でなければ登録できない" do
-      @user.save
-      another_user = FactoryBot.build(:user)
-      another_user.email = @user.email
-      another_user.valid?
-      expect(another_user.errors.full_messages).to include('Email has already been taken')
-    end
-
     it "passwordが空では登録できない" do
       @user.password = ''
       @user.valid?

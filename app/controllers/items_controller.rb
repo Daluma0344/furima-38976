1680e1f1ca93a0b930424469a7b73654d3a5ac95
Daluma0344 <japan_masa11@icloud.com>
2023-04-04 18:31:12 +0900

    destroyアクションの指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
 
   def index
@@ -38,6 +38,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @item.user_id != current_user.id
+      redirect_to root_path
+    end
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
 

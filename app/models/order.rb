c2ee6a1e64a9c42cb5749128f9cc69967b04d716
Daluma0344 <japan_masa11@icloud.com>
2023-04-06 16:42:32 +0900

    指摘修正1

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,15 +14,14 @@ class Order
     validates :house_number
     validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
 
-  validates :token
-end
+    validates :token
+  end
 
-validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+  validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
 
-def save
+  def save
    order = PurchaseRecord.create(user_id: user_id, item_id: item_id)
    ShoppingInformation.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, house_number: house_number, building_name: building_name, telephone_number: telephone_number, purchase_record_id: order.id)
-end
-
+  end
 end
 
